b9f840ca71efd138577ae8f61af88adf707bde9f
Marxu <fuhaojun@52tt.com>
2024-07-17 20:43:30 +0800

    feat: 加个时钟和背景色

--- projects/py_game/alien_attack.py
+++ projects/py_game/alien_attack.py
@@ -6,9 +6,13 @@ import pygame
 class AlienAttack:
     def __init__(self):
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption('Alien Attack')
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
 
     def start_game(self):
         while True:
@@ -16,7 +20,10 @@ class AlienAttack:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.bg_color)
+
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
