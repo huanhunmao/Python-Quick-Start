f477cd7ce4fea8e260d1e59b981245d1a8682267
Marxu <fuhaojun@52tt.com>
2024-07-18 13:56:36 +0800

    feat: 在 settings 里加一个速度 并使用这个速度

--- projects/py_game/ship.py
+++ projects/py_game/ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -13,15 +14,21 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # 存 x 位置
+        self.x = float(self.rect.x)
         # Movement flag; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # 更新 rect 里的 x 值
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
