b0e605fc698f0d0366cb1e3504ad7706ccf1ef69
Marxu <fuhaojun@52tt.com>
2024-07-18 11:39:57 +0800

    feat: 动起来，船可以左右移动

--- projects/py_game/alien_attack.py
+++ projects/py_game/alien_attack.py
@@ -21,6 +21,7 @@ class AlienAttack:
     def start_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -28,6 +29,17 @@ class AlienAttack:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         self.screen.fill(self.bg_color)
--- projects/py_game/ship.py
+++ projects/py_game/ship.py
@@ -13,6 +13,15 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        # Movement flag; start with a ship that's not moving.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw the ship at its current location."""
