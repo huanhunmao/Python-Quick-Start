527f5ac3c28d513a200f68d778c7bd98c394bf81
Marxu <fuhaojun@52tt.com>
2024-07-19 15:38:20 +0800

    feat: 加速分数

--- projects/py_game/settings.py
+++ projects/py_game/settings.py
@@ -20,6 +20,7 @@ class Settings:
 
         # 加速
         self.speedup_scale = 1.1
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -39,3 +40,6 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
