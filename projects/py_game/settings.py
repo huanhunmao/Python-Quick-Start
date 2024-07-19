ce2220f68621eb197e1dc7834058b76a06896cbe
Marxu <fuhaojun@52tt.com>
2024-07-19 16:38:02 +0800

    feat: 恢复 settings 数据

--- projects/py_game/settings.py
+++ projects/py_game/settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
