f8c81ef0e055761fb92893753ffaecedac8f9589
Marxu <fuhaojun@52tt.com>
2024-07-19 11:05:54 +0800

    feat: 点击了 Play，改变游戏状态为激活，开始游戏

--- projects/py_game/settings.py
+++ projects/py_game/settings.py
@@ -18,7 +18,7 @@ class Settings:
         self.bullet_allowed = 3
 
         # Alien settings
-        self.alien_speed = 30.0
+        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         # 1代表👉 -1代表👈
         self.fleet_direction = 1
