31a2b1f39b1fea1c029dcd450e69c32c60657e92
Marxu <fuhaojun@52tt.com>
2024-07-20 15:41:56 +0800

    feat: 清理轴

--- projects/generate_data/rw_visual.py
+++ projects/generate_data/rw_visual.py
@@ -18,6 +18,10 @@ while True:
     ax.scatter(0, 0, c='green', edgecolor='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
                edgecolor='none', s=100)
+    # 清理轴
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     # keep_running = input('Make another walk? (y/n): ')
