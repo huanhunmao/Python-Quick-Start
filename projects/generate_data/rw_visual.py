306c2c367eaea2cac4a3fc990ae701bb3c8f71e6
Marxu <fuhaojun@52tt.com>
2024-07-20 15:24:28 +0800

    feat: 程序还会继续生成多个随机行走 while

--- projects/generate_data/rw_visual.py
+++ projects/generate_data/rw_visual.py
@@ -1,13 +1,19 @@
 import  matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# 进行随机walk
-rw = RandomWalk()
-rw.fill_walk()
+# 程序还会继续生成多个随机行走
+while True:
+    # 进行随机walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# 绘制walk点
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+    # 绘制walk点
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
