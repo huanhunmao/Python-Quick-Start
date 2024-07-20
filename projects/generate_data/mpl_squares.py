b72aa85daa94b90e937fe2dbc79ba50ade1182e1
Marxu <fuhaojun@52tt.com>
2024-07-20 14:58:02 +0800

    feat: 画点，加一点样式

--- projects/generate_data/mpl_squares.py
+++ projects/generate_data/mpl_squares.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+# ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=100)
 
 # 设置标题和标签轴
 ax.set_title('Square Numbers', fontsize=24)
