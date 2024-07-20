51551e52541b9b726be69053954f629f66c839c5
Marxu <fuhaojun@52tt.com>
2024-07-20 14:50:01 +0800

    feat: 更改标签类型和线粗细

--- projects/generate_data/mpl_squares.py
+++ projects/generate_data/mpl_squares.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3)
 
-ax.plot(squares)
+# 设置标题和标签轴
+ax.set_title('Square Numbers', fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Value', fontsize=14)
+
+# 设置刻度标签大小
+ax.tick_params(labelsize=14)
 
 plt.show()
