8e784e1f54408d29b380f8955ef80dedd65eefa1
Marxu <fuhaojun@52tt.com>
2024-07-20 15:01:58 +0800

    feat: 自动算数

--- projects/generate_data/mpl_squares.py
+++ projects/generate_data/mpl_squares.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 # ax.plot(input_values, squares, linewidth=3)
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # 设置标题和标签轴
 ax.set_title('Square Numbers', fontsize=24)
@@ -18,5 +18,8 @@ ax.set_ylabel('Square of Value', fontsize=14)
 
 # 设置刻度标签大小
 ax.tick_params(labelsize=14)
+
+# 设置每个轴范围
+ax.axis([0, 1100, 0, 1_100_000])
 
 plt.show()
