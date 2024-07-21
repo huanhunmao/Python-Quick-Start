e1402dfef8f2a941cdca8cc9268e5800be9c31dd
Marxu <fuhaojun@52tt.com>
2024-07-21 09:16:04 +0800

    feat: 投掷2个骰子🎲

--- projects/generate_data/die_visual.py
+++ projects/generate_data/die_visual.py
@@ -1,22 +1,25 @@
 from die import Die
 import plotly.express as px
 
-die = Die()
+# 设置两个骰子
+die_1 = Die()
+die_2 = Die()
 
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # 分析结果
 frequencies = []
-poss_results = range(1, die.num_sides+1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # 制作直方图
-title = "Results of Rolling One D6 1,000 Times"
+title = "Results of Rolling Two D6 1,000 Times"
 labels = {'x': 'Results', 'y': 'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 fig.show()
