f388ed633dd92a8dffd5a9dc7e971930dbd4de50
Marxu <fuhaojun@52tt.com>
2024-07-21 15:55:35 +0800

    feat: 每个item 仓库基本信息打印出来

--- projects/working_with_apis/python_repos_copy.py
+++ projects/working_with_apis/python_repos_copy.py
@@ -11,23 +11,11 @@ r = requests.get(url, headers=headers)
 response_dict = r.json()
 
 repo_dicts = response_dict['items']
+print("\nSelected information about each repository:")
 
-# 检查第一个存储库
-repo_dict = repo_dicts[0]
-print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
-
-# Selected information about first repository:
-# Name: public-apis
-# Owner: public-apis
-# Stars: 302963
-# Repository: https://github.com/public-apis/public-apis
-# Created: 2016-03-20T23:49:42Z
-# Updated: 2024-07-21T07:40:15Z
-# Description: A collective list of free APIs
+for repo_dict in repo_dicts:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
