3e5ecefff25ccad8c4c4ef4ea640eb8f2e439144
Marxu <fuhaojun@52tt.com>
2024-07-21 16:18:43 +0800

    feat: x 轴展示的东西 改为可点击链接

--- projects/working_with_apis/python_repos_visual.py
+++ projects/working_with_apis/python_repos_visual.py
@@ -15,9 +15,12 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Process repository information.
 repo_dicts = response_dict['items']
-repo_names, stars, hover_texts = [], [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
 
 # 构建悬停文本
@@ -29,8 +32,6 @@ for repo_dict in repo_dicts:
 # Make visualization.
 title = "Most-Starred Python Projects on GitHub"
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
-
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels,
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,
         hover_name=hover_texts)
 fig.show()
