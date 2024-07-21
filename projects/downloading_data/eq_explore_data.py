7e4ebb5d0db9ecd77b2cc0a5d70978aa47c7b4b4
Marxu <fuhaojun@52tt.com>
2024-07-21 11:34:40 +0800

    feat: 列出所有 地震

--- projects/downloading_data/eq_explore_data.py
+++ projects/downloading_data/eq_explore_data.py
@@ -6,7 +6,10 @@ path = Path('eq_data/eq_data_1_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
-# 创建一个更可读的数据文件版本
-path = Path('eq_data/readable_eq_data.geojson')
-readable_contents = json.dumps(all_eq_data, indent=4 )
-path.write_text(readable_contents)
+# # 创建一个更可读的数据文件版本
+# path = Path('eq_data/readable_eq_data.geojson')
+# readable_contents = json.dumps(all_eq_data, indent=4 )
+# path.write_text(readable_contents)
+
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
