5b9c46e779098315adcdee855defcee110f432f1
Marxu <fuhaojun@52tt.com>
2024-07-21 11:36:54 +0800

    feat: 提取地震等级

--- projects/downloading_data/eq_explore_data.py
+++ projects/downloading_data/eq_explore_data.py
@@ -12,4 +12,9 @@ all_eq_data = json.loads(contents)
 # path.write_text(readable_contents)
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
