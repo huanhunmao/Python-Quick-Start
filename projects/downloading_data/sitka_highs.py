72ae8566d6a0ad0d160c4be7bc0ea7577ed3a2f2
Marxu <fuhaojun@52tt.com>
2024-07-21 10:50:55 +0800

    feat: 画出两根线

--- projects/downloading_data/sitka_highs.py
+++ projects/downloading_data/sitka_highs.py
@@ -10,18 +10,24 @@ lines = path.read_text(encoding='utf-8').splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, color='red')
+# Plot highs with a blue line
+ax.plot(dates, highs, label='Highs', color='blue', linewidth=2)
 
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+# Plot lows with a red line
+ax.plot(dates, lows, label='Lows', color='red', linewidth=2)
+
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
