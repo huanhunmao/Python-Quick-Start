448b7b3833992a26f31d830dedb22558b5cdd8c3
Marxu <fuhaojun@52tt.com>
2024-07-22 15:02:51 +0800

    feat: 新增 topic page

--- projects/getting_started_with_django/learning_logs/models.py
+++ projects/getting_started_with_django/learning_logs/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-
-# Create your models here.
 class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -12,15 +10,12 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
-
-class Meta:
-    verbose_name_plural = 'entries'
+    class Meta:
+        verbose_name_plural = 'entries'
 
     def __str__(self):
         """Return a simple string representing the entry."""
-
         return f"{self.text[:50]}..."
--- projects/getting_started_with_django/learning_logs/views.py
+++ projects/getting_started_with_django/learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 
 # Create your views here.
@@ -23,3 +24,19 @@ def topics_detail(request, topic_id):
     except Topic.DoesNotExist:
         return render(request, 'not_found.html')
 
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+         form = TopicForm()
+    else:
+        # POST data submitted; process data.
+         form = TopicForm(data=request.POST)
+         if form.is_valid():
+             form.save()
+             return redirect('learning_logs:topics')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'new_topic.html', context)
