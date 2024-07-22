f63cc9f736d081741925fc0faa326719272e62ea
Marxu <fuhaojun@52tt.com>
2024-07-22 16:55:39 +0800

    feat: 重置一下 项目 添加 登陆和注册功能

--- projects/getting_started_with_django/learning_logs/forms.py
+++ projects/getting_started_with_django/learning_logs/forms.py
@@ -7,8 +7,7 @@ class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
-        labels = {'text': 'Enter Topic Text:'}
-
+        labels = {'text': ''}
 
 class EntryForm(forms.ModelForm):
     class Meta:
--- projects/getting_started_with_django/ll_project/urls.py
+++ projects/getting_started_with_django/ll_project/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for ll_project project.
+"""ll_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('learning_logs.urls'))
+    path('', include('learning_logs.urls')),
 ]
