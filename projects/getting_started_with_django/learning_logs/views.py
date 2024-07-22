8b58d021275d1ba9d4351450f976d563ca9df84c
Marxu <fuhaojun@52tt.com>
2024-07-22 11:01:30 +0800

    feat: 加一个 传递 id 可以展示 detail 信息 并没有时 展示 not-found page

--- projects/getting_started_with_django/learning_logs/views.py
+++ projects/getting_started_with_django/learning_logs/views.py
@@ -11,3 +11,15 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'topics.html', context)
+
+
+def topics_detail(request, topic_id):
+    """Show a single topic and all its entries."""
+    try:
+        topic = Topic.objects.get(id=topic_id)
+        entries = topic.entry_set.order_by('-date_added')
+        context = {'topic': topic, 'entries': entries}
+        return render(request, 'topics_detail.html', context)
+    except Topic.DoesNotExist:
+        return render(request, 'not_found.html')
+
